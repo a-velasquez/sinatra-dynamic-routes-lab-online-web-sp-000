0a5e5d5859d22c137d8b2327b8fdce7938976811
Alex Velasquez <jtinnesco@gmail.com>
2020-02-10 01:54:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     @number = params[:number].to_i
 
     string = ""
-    @number.times { string << @phrase } 
+    @number.times { string << @phrase+"\n" } 
     string
   end
 
