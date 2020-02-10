8d4df5982c18d1a1e6ea3f564c78a25abf90ee03
Alex Velasquez <jtinnesco@gmail.com>
2020-02-10 02:28:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,5 +29,8 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1   = params[:number1]
     @number2   = params[:number2]
+
+    if @operation == "add"
+      "#{number1+number2}".to_s
   end
 end
